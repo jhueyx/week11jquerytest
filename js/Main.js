c0ce20d67eaa3cd0f1d671e7c8497c8172e552f4
Jason Huey <jason.huey1@gmail.com>
2018-07-31 12:38:39 -0700

    first commit

---
+++ js/Main.js
@@ -0,0 +1,13 @@
+$("#hide").click(function() {
+	$("#img").hide();
+}
+	);
+$("#show").click(function() {
+	$("#img").show();
+}
+	);
+
+$("#toggleImg").click(function(){
+	$("#img").toggle()
+}
+	)
